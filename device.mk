738cac10538d0a269d3510b030d88deb9d9fba07
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2023-04-20 18:31:01 +0530

    lavender: remove hardware/google/pixel

--- device.mk
+++ device.mk
@@ -31,8 +31,7 @@ BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/xiaomi/lavender \
-                            hardware/google/interfaces \
-                            hardware/google/pixel
+                            hardware/google/interfaces 
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
