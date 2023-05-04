c2bba50084d88907a131eaa4a3232c9bc6920049
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2023-05-04 17:20:17 +0530

    lavender: BoardConfig: Add some flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,10 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
-TARGET_KERNEL_LLVM_BINUTILS := false
+TARGET_KERNEL_LLVM_BINUTILS := true
+TARGET_KERNEL_CLANG_VERSION := clang-nexus14
+TARGET_CLANG_PREBUILTS_VERSION := clang-nexus14
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-nexus14
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm660
